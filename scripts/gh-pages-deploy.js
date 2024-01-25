40aa39673b8f3215398a342a655053d1d2db98a2
BCaven <blakecaven@outlook.com>
2024-01-24 21:18:59 -0500

    require main

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,16 +1,25 @@
 /* eslint-disable no-console */
 const execa = require("execa");
 const fs = require("fs");
+import {$} from 'execa';
+
 (async () => {
-  try {
-    await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    // eslint-disable-next-line no-console
-    console.log("Building started...");
-    await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", ["--work-tree", folderName, "add", "--all"]);
-    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    const branch = await $`git branch --show-current`;
+    // make sure we are on the main branch
+    if (branch != "main") {
+        console.log("Please run this command from the main branch");
+        process.exit(1);
+    }
+    try {
+        await execa("git", ["checkout", "main"]);
+        await execa("git", ["checkout", "--orphan", "gh-pages"]);
+        // eslint-disable-next-line no-console
+        console.log("Building started...");
+        await execa("npm", ["run", "build"]);
+        // Understand if it's dist or build folder
+        const folderName = fs.existsSync("dist") ? "dist" : "build";
+        await execa("git", ["--work-tree", folderName, "add", "--all"]);
+        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
     await execa("rm", ["-r", folderName]);
@@ -20,6 +29,8 @@ const fs = require("fs");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
+    // if we fail we want to go back to the branch we started on
+    await execa("git", ["branch", branch]);
     process.exit(1);
   }
 })();
