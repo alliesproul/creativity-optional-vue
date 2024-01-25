1c7864dcefbebfbaeeb7aa9f51c47613e5edd822
BCaven <blakecaven@outlook.com>
2024-01-24 22:52:19 -0500

    fix where the site is deployed to

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/creativityoptional/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/creativity-optional-vue/" : "/",
 };
