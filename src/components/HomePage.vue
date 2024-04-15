08fdbd327b23fead364d2bdd830b17d605b72a08
TorpidTuna <abayanzu@nd.edu>
2024-04-15 02:37:29 -0400

    CO is now public

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -11,7 +11,44 @@
 
       <p class="text-h7 font-weight-medium">We are a team making an open source project to turn audio into meaningful and (hopefully) pretty video!</p>
 
-      <div class="py-4" />
+      <div class="py-9" />
+
+
+      <v-sheet
+            color="#21212c"
+            variant="flat"
+            width="100%"
+            rounded
+        >
+            <div class="py-3" />
+            <v-card-title class="text-h4 font-weight-bold">Announcements</v-card-title>
+            <div class="py-3" />
+            <v-row class="d-flex align-center justify-center">
+
+                <v-col cols="9">
+                    <v-card
+                        color="#ffdf32"
+                        variant="flat"
+                        class="mx-auto"
+                    >
+                        <v-card-title class="text-h6 font-weight-bold">Public Release!</v-card-title>
+                        <v-card-item>
+                          Creativity Optional is now available to the unwashed masses! Contributors welcome! Here's an introductory video!<br>
+                          <video width="800" controls>
+                            <source src="@/assets/intro.mp4">
+                            Your browser does not support the video tag.
+                          </video><br>
+                          <a href="https://discord.com/invite/qN9BhGK36U">Discord</a><br>
+                          <a href="https://github.com/BCaven/creativity-optional">GitHub</a>
+                        </v-card-item>
+                    </v-card>
+                </v-col>
+            </v-row>
+            <div class="py-5" />
+        </v-sheet>
+
+
+      <div class="py-9" />
 
       <h2 class="text-h3 font-weight-bold">Explore</h2>
 
