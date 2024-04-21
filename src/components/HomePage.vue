ae47b413a1f23d3f03dfc5a0cbd7ee9d45fdcea3
alliesproul <156840994+alliesproul@users.noreply.github.com>
2024-04-21 13:03:40 -0400

    link bottom buttons

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -58,19 +58,22 @@
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>To see what we have so far:</v-card-item>
-            <v-card-title>SIMULATION</v-card-title>
+            <router-link to="/simulation">
+              <v-card-title>SIMULATION</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>For more info on our project:</v-card-item>
-            <v-card-title>INFO</v-card-title>
+            <router-link to="/info">
+              <v-card-title>INFO</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>If you want to contact us:</v-card-item>
-            <v-card-title>CONTACT</v-card-title>
+            <router-link to="/contact">
+              <v-card-title>CONTACT</v-card-title>
           </v-card>
         </v-col>
       </v-row>
