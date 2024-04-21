dbbaee08613f1e2fd08049c53a1d7cc4025dd36d
alliesproul <156840994+alliesproul@users.noreply.github.com>
2024-04-21 16:10:46 -0400

    Update HomePage.vue update buttons

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,9 @@
+const routes = [
+  { path: '/contact', component: ContactPage },
+  { path: '/info', component: InfoPage },
+  { path: '/simulation', component: SimulationPage },
+]
+
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
@@ -58,21 +64,21 @@
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>To see what we have so far:</v-card-item>
-            <router-link to="/SimulationPage.vue">
+            <RouterLink to="/simulation">SIMULATIONe</RouterLink>
               <v-card-title>SIMULATION</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>For more info on our project:</v-card-item>
-            <router-link to="/InfoPage.vue">
+            <RouterLink to="/info">INFORMATION</RouterLink>
               <v-card-title>INFO</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>If you want to contact us:</v-card-item>
-            <router-link to="/ContactPage">
+            <RouterLink to="/contact">CONTACT</RouterLink>
               <v-card-title>CONTACT</v-card-title>
           </v-card>
         </v-col>
