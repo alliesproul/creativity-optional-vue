5ca56c8bf0df492318bb978078061ce9b2e8ba41
alliesproul <156840994+alliesproul@users.noreply.github.com>
2024-04-21 13:14:32 -0400

    Update HomePage.vue buttons

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -58,21 +58,21 @@
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>To see what we have so far:</v-card-item>
-            <router-link to="/simulation">
+            <router-link to="/SimulationPage.vue">
               <v-card-title>SIMULATION</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>For more info on our project:</v-card-item>
-            <router-link to="/info">
+            <router-link to="/InfoPage.vue">
               <v-card-title>INFO</v-card-title>
           </v-card>
         </v-col>
         <v-col cols="3">
           <v-card color="#3253ff">
             <v-card-item>If you want to contact us:</v-card-item>
-            <router-link to="/contact">
+            <router-link to="/ContactPage">
               <v-card-title>CONTACT</v-card-title>
           </v-card>
         </v-col>
